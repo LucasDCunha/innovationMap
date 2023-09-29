34ab28235a62013ec241e1ec5a35fb2ef8733521
LucasDC <46499843+LucasDCunha@users.noreply.github.com>
2023-09-29 15:43:06 -0300

    wip

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl'
+
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+  }
+}
--- app/models/get.rb
+++ app/models/get.rb
@@ -1,5 +1,4 @@
 class Get < ApplicationRecord
-    geocoded_by :address
-
+    geocoded_by :endereco
     after_validation :geocode, if: :will_save_change_to_address?
 end
